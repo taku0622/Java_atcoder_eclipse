c6299434e941b3564fa72413faaf334a885ea41a
taku0622 <hayatsutkm@gmail.com>
2021-06-13 17:19:07 +0900

    'done'

---
+++ src/jp/atcoder/Hello.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package jp.atcoder;
+
+/**
+ * @author C0117253
+ *
+ */
+public class Hello {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hello");
+	}
+
+}
