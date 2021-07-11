5cb2f9e55fcb955ac7629e134b14b9f9989b5ef7
taku0622 <hayatsutkm@gmail.com>
2021-07-11 19:32:09 +0900

    done

--- src/jp/atcoder/ABC200_A.java
+++ src/jp/atcoder/ABC200_A.java
@@ -9,17 +9,20 @@ public class ABC200_A {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		System.out.println(str.length());
-		int i1 = Integer.parseInt(str.substring(0, 2));
-		int i2 = Integer.parseInt(str.substring(2, 4));
-		String ans = "NA\r\n";
-		if((i1 > 0 && i1 < 13) && (i2 > 0 && i2 < 13)) {
-			ans = "AMBIGUOUS\r\n";
-		} else if (i1 > 0 && i1 < 13) {
-			ans = "MMYY\r\n";
-		} else if (i2 > 0 && i2 < 13) {
-			ans = "YYMM\r\n";
+		int i1;
+		int i2;
+		if(str.length() < 3) {
+			System.out.println(1);
+			System.exit(0);
 		}
+		if(str.length() == 3) {
+			i1 = Integer.parseInt(str.substring(0, 1));
+			i2 = Integer.parseInt(str.substring(1, 3));
+		} else {
+			i1 = Integer.parseInt(str.substring(0, 2));
+			i2 = Integer.parseInt(str.substring(2, 4));
+		}
+		int ans = i2 > 0 ? i1 + 1 : i1;
 		System.out.println(ans);
 	}
 
