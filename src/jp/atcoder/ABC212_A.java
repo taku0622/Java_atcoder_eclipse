f092c2c3075afab7f6b707174fd76bb1ae4834ee
taku0622 <hayatsutkm@gmail.com>
2021-08-21 16:10:19 +0900

    done

--- src/jp/atcoder/ABC212_A.java
+++ src/jp/atcoder/ABC212_A.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ABC211_A {
+public class ABC212_A {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,9 +12,16 @@ public class ABC211_A {
 		String[] sa = str.split(" ");
 		int[] il = new int[sa.length];
 		for(int i=0;i<sa.length;i++) { il[i] = Integer.parseInt(sa[i]); }
-		double ans = 0.0;
-		ans = (il[0]-il[1])/3 + il[1];
-
+		String ans;
+		if(il[0] == 0 || il[1] == 0) {
+			if(il[0] > 0) {
+				ans = "Gold";
+			}else {
+				ans = "Silver";
+			}
+		}else {
+			ans = "Alloy";
+		}
 		System.out.println(ans);
 	}
 
