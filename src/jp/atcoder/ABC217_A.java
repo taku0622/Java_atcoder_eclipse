97e0b27f5fd0c5e984f790d8bce753a3c39ce3af
taku0622 <hayatsutkm@gmail.com>
2021-11-04 22:31:19 +0900

    done

---
+++ src/jp/atcoder/ABC217_A.java
@@ -0,0 +1,20 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ABC217_A {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String[] sa = str.split(" ");
+//		int[] il = new int[sa.length];
+//		for(int i=0;i<sa.length;i++) { il[i] = Integer.parseInt(sa[i]); }
+		String ans = (sa[0].compareTo(sa[1]))==-1 ? "Yes" : "No";
+		System.out.println(ans);
+	}
+
+
+}
