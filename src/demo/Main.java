756845caf102af7335aa06e17a9e4e5b1ff2293c
taku0622 <hayatsutkm@gmail.com>
2022-03-27 04:58:28 +0900

    done

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -3,13 +3,14 @@ package demo;
 import java.util.ArrayList;
 
 import entity.Member;
+import service.MemberService;
 import service.MemberServiceImpl;
 
 public class Main {
 
 	public static void main(String[] args) {
 //		MemberServiceImpl service = new  MemberServiceImpl();
-		MemberServiceImpl service = MemberServiceImpl.getInstance();
+		MemberService service = MemberServiceImpl.getInstance();
 		System.out.println(service.greet(2));
 
 		System.out.println(service.getAll());
