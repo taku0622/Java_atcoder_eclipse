1542db6e63a60bab55aaf718fb6acbdee9a57914
taku0622 <hayatsutkm@gmail.com>
2022-04-10 23:58:26 +0900

    done

---
+++ src/jp/atcoder/ABC246_A.java
@@ -0,0 +1,36 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC246_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		goodMorning(il);
+	}
+	private static void goodMorning(ArrayList<Integer> il) {
+		ArrayList<String> sl = new ArrayList<String>();
+		for(int i:il) {
+			sl.add(String.format("%02d", i));
+		}
+		int a = Integer.parseInt(sl.get(0) + sl.get(1));
+		int b = Integer.parseInt(sl.get(2) + sl.get(3));
+
+		String ans = a <= b ? "Takahashi" : "Aoki";
+		System.out.println(ans);
+	}
+}
---
+++ src/jp/atcoder/ABC247_A.java
@@ -0,0 +1,35 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC247_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split("");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		moveRight(il);
+	}
+	private static void moveRight(ArrayList<Integer> il) {
+		System.out.println(il);
+		il.add(0);
+		for(int i=4;i>0;i--) {
+			il.set(i,il.get(i-1));
+		}
+		il.remove(4);
+		System.out.println(il);
+		String ans = ""
+	}
+}
