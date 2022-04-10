d890f62bb417f5c17264943d7373bf19ff053fbf
taku0622 <hayatsutkm@gmail.com>
2022-04-11 02:10:51 +0900

    done

--- src/jp/atcoder/ABC246_A.java
+++ src/jp/atcoder/ABC246_A.java
@@ -2,35 +2,34 @@ package jp.atcoder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class ABC246_A {
 
 	public static void main(String[] args) {
-		String str1;
+		String str1,str2,str3;
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			str1 = br.readLine();
+			str2 = br.readLine();
+			str3 = br.readLine();
 		}catch (Exception e) {
 			System.out.println("Input Error!!");
 			return;
 		}
-		String[] sa = str1.split(" ");
-		ArrayList<Integer> il = new ArrayList<Integer>();
-		for(String s:sa) {
-			il.add(Integer.parseInt(s));
-		}
-		goodMorning(il);
+		String[] sa1 = str1.split(" ");
+		String[] sa2 = str2.split(" ");
+		String[] sa3 = str3.split(" ");
+		String[][] arr = {sa1,sa2,sa3};
+		forPoints(arr);
 	}
-	private static void goodMorning(ArrayList<Integer> il) {
-		ArrayList<String> sl = new ArrayList<String>();
-		for(int i:il) {
-			sl.add(String.format("%02d", i));
-		}
-		int a = Integer.parseInt(sl.get(0) + sl.get(1));
-		int b = Integer.parseInt(sl.get(2) + sl.get(3));
-
-		String ans = a <= b ? "Takahashi" : "Aoki";
-		System.out.println(ans);
+	private static void forPoints(String[][] arr) {
+		String[] ans = {"0","0"};
+		if(arr[0][0].equals(arr[1][0])) ans[0] = arr[2][0];
+		if(arr[0][0].equals(arr[2][0])) ans[0] = arr[1][0];
+		if(arr[1][0].equals(arr[2][0])) ans[0] = arr[0][0];
+		if(arr[0][1].equals(arr[1][1])) ans[1] = arr[2][1];
+		if(arr[0][1].equals(arr[2][1])) ans[1] = arr[1][1];
+		if(arr[1][1].equals(arr[2][1])) ans[1] = arr[0][1];
+		System.out.println(ans[0] + " " + ans[1]);
 	}
 }
