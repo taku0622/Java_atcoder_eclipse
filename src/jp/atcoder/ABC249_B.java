f037eea23c37a80bd7258d8be6d4f8875ade0346
taku0622 <hayatsutkm@gmail.com>
2022-05-28 20:30:55 +0900

    done

--- src/jp/atcoder/ABC249_B.java
+++ src/jp/atcoder/ABC249_B.java
@@ -2,7 +2,7 @@ package jp.atcoder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ABC249_B {
 
@@ -15,31 +15,29 @@ public class ABC249_B {
 			System.out.println("Input Error!!");
 			return;
 		}
-		String[] sa = str1.split(" ");
-		ArrayList<Integer> il = new ArrayList<Integer>();
-		for(String s:sa) {
-			il.add(Integer.parseInt(s));
-		}
-		jogging(il);
+		char[] ca = str1.toCharArray();
+
+		perfectString(ca);
 	}
-	private static void jogging (ArrayList<Integer> il) {
-		// a:0 b:1 c:2 d:3 e:4 f:5 g:6
-		// takahashi
-		int a = (il.get(6) / (il.get(0)+il.get(2))*il.get(0))*il.get(1);
-		int b = il.get(6)%(il.get(0)+il.get(2)) > il.get(0) ?
-				 il.get(0) * il.get(1) : il.get(6)%(il.get(0)+il.get(2))*il.get(1);
-//		System.out.println("a="+a+" b="+b);
-		// aoki
-		int c = (il.get(6) / (il.get(3)+il.get(5))*il.get(3))*il.get(4);
-		int d = il.get(6)%(il.get(3)+il.get(5)) > il.get(3) ?
-				 il.get(3) * il.get(4) : il.get(6)%(il.get(3)+il.get(5))*il.get(4);
-//		System.out.println("c="+c+" d="+d);
-		if(a+b == c+d) {
-			System.out.println("Draw");
-		}else if (a+b > c+d) {
-			System.out.println("Takahashi");
-		}else {
-			System.out.println("Aoki");
+	private static void perfectString(char[] ca) {
+		HashMap<Character, Boolean> hMap = new HashMap<Character, Boolean>();
+		boolean judgeLower = false;
+		boolean judgeUpper = false;
+		for(char c : ca) {
+			if(hMap.containsKey(c)) {
+				System.out.println("No");
+				return;
+			}
+			if(Character.isLowerCase(c)) judgeLower = true;
+			if(Character.isUpperCase(c)) judgeUpper = true;
+			hMap.put(c, true);
+//			System.out.println(hMap);
 		}
+		if (judgeLower && judgeUpper) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+
 	}
 }
