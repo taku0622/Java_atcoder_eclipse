cf75b64dfc1704a93607692ac47090ea9a89edd1
taku0622 <hayatsutkm@gmail.com>
2022-05-29 19:51:04 +0900

    done

--- src/silver/Sample.java
+++ src/silver/Sample.java
@@ -2,8 +2,20 @@ package silver;
 
 public class Sample {
 
-	public static void main(String... args) {
-		System.out.println(args.length);
+	public static void main(String[] args) {
+		int a = 267;
+
+		int b = 0413;
+
+		int c = 0x10B;
+
+		int d = 0b100001011;
+
+//		int e = 0827;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
