4f925f0b4dbf73fa8b77364abece221a7df1b950
taku0622 <hayatsutkm@gmail.com>
2022-06-05 20:27:57 +0900

    done

--- src/jp/atcoder/ABC254_A.java
+++ src/jp/atcoder/ABC254_A.java
@@ -15,20 +15,14 @@ public class ABC254_A {
 			System.out.println("Input Error!!");
 			return;
 		}
-		String[] sa = str1.split(" ");
+		String[] sa = str1.split("");
 		ArrayList<Integer> il = new ArrayList<Integer>();
 		for(String s:sa) {
 			il.add(Integer.parseInt(s));
 		}
-		median(il);
+		lastTwoDigits(il);
 	}
-	private static void median(ArrayList<Integer> il) {
-		int a = il.get(0);
-		int b = il.get(1);
-		int c = il.get(2);
-		boolean asc = a <= b && b <= c ? true : false;
-		boolean desc = c <= b && b <= a ? true : false;
-		String ans = asc || desc ? "Yes" : "No";
-		System.out.println(ans);
+	private static void lastTwoDigits(ArrayList<Integer> il) {
+		System.out.println("" + il.get(1) + il.get(2));
 	}
 }
