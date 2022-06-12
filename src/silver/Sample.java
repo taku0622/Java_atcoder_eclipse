a37d368379918e58fe72c9c3146b7317ad95ce1b
taku0622 <hayatsutkm@gmail.com>
2022-06-12 10:17:50 +0900

    done

--- src/silver/Sample.java
+++ src/silver/Sample.java
@@ -3,19 +3,24 @@ package silver;
 public class Sample {
 
 	public static void main(String[] args) {
-		int a = 267;
+//		int a = 267;
+//
+//		int b = 0413;
+//
+//		int c = 0x10B;
+//
+//		int d = 0b100001011;
+//
+////		int e = 0827;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
 
-		int b = 0413;
-
-		int c = 0x10B;
-
-		int d = 0b100001011;
-
-//		int e = 0827;
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
+		String string = "abcde";
+		System.out.println(string.indexOf('c'));	// 2
+		System.out.println(string.indexOf("cd"));	// 2
+		System.out.println(string.indexOf("cdf"));	// -1
 	}
 
 }
