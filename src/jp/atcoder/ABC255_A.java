9b46541a012b0a768704dfe9e65bee29d5a25d52
taku0622 <hayatsutkm@gmail.com>
2022-06-14 23:49:45 +0900

    done

---
+++ src/jp/atcoder/ABC255_A.java
@@ -0,0 +1,28 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC255_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split("");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		lastTwoDigits(il);
+	}
+	private static void lastTwoDigits(ArrayList<Integer> il) {
+		System.out.println("" + il.get(1) + il.get(2));
+	}
+}
