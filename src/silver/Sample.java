d86685b85bb0b92031f73dfb81b4e979537b48fe
taku0622 <hayatsutkm@gmail.com>
2022-06-18 14:18:22 +0900

    done

--- src/silver/Sample.java
+++ src/silver/Sample.java
@@ -48,6 +48,15 @@ public class Sample {
 //		} while (++num < 10);
 //		System.out.println(num);
 
+		int total = 0;
+		a: for (int i = 0; i < 5; i++) {
+			b: for (int j = 0; j < 5; j++) {
+				if (i % 2 == 0) continue a;
+				if (3 < j) break b;
+				total += j;
+				}
+			}
+		System.out.println(total);
 	}
 	private static void period() {
 		System.out.print(",");
