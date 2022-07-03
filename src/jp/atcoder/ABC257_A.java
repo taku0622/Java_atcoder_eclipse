521612d543f434576c7e9aabffe65704985d779d
taku0622 <hayatsutkm@gmail.com>
2022-07-03 18:42:57 +0900

    done

--- src/jp/atcoder/ABC257_A.java
+++ src/jp/atcoder/ABC257_A.java
@@ -25,6 +25,17 @@ public class ABC257_A {
 	private static void AtoZString2(ArrayList<Integer> il) {
 		String abcString ="ABCDEFGHIJKLMNOPQRSYUVWXYZ";
 		String[] abcStrings = abcString.split("");
-		System.out.println(abcStrings.length);
+		String anString = "";
+		int count = 0;
+		for(int i = 0;i < abcStrings.length;i++) {
+			for(int j = 0;j < il.get(0);j++) {
+				anString += abcStrings[0];
+				count++;
+				if(count==il.get(1)) {
+					System.out.println(abcStrings[i]);
+					// System.out.println(anString);
+				}
+			}
+		}
 	}
 }
