800cd325a779b9ad3ee2eb8838ad883973b74f55
taku0622 <hayatsutkm@gmail.com>
2022-07-16 19:31:21 +0900

    done

---
+++ src/jp/atcoder/ABC259_A.java
@@ -0,0 +1,39 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC259_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		growthRecord(il);
+	}
+	private static void growthRecord(ArrayList<Integer> il) {
+		int n = il.get(0);
+		int m = il.get(1);
+		int x = il.get(2);
+		int t = il.get(3);
+		int d = il.get(4);
+		int ans = 0;
+		if(n >= m && m >= x) {
+			ans = t;
+		}else {
+			ans = ((n-m) * d) - t;
+		}
+		System.out.println(ans);
+	}
+}
