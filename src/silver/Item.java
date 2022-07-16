9f1d04e1b74698852327c19976743084f7e8ce57
taku0622 <hayatsutkm@gmail.com>
2022-07-16 19:01:34 +0900

    done

--- src/silver/Item.java
+++ src/silver/Item.java
@@ -1,11 +1,8 @@
 package silver;
 
 public class Item {
-	public Item() {
-		System.out.println("hello");
-		this(0, 0);	// コンパイルエラーになる
-	}
-	public Item(int a, int b) {
-		System.out.println(a + b);
+	int num;
+	public Item(int num) {
+		this.num = num;
 	}
 }
