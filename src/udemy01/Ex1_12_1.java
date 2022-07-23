f5578f2d53976c69d3db9f5247247441dd3e9d20
taku0622 <hayatsutkm@gmail.com>
2022-07-23 19:42:57 +0900

    done

---
+++ src/udemy01/Ex1_12_1.java
@@ -0,0 +1,20 @@
+package udemy01;
+class Ex1_12_1 {
+	public static void main (String[] args) {
+		int point = Integer.parseInt(args[0]);
+		String ans = "red";
+		if(point >= 0 && point <= 100) {
+			if(point >= 80) {
+				ans = "excelent";
+				if(point == 100) ans += "\npass home work!";
+			}else if(point >= 60) {
+				ans = "good";
+			}else {
+				ans = "red point";
+			}
+		}else {
+			ans = "no point";
+		}
+		System.out.println(ans);
+	}
+}
