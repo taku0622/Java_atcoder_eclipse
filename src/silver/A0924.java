ac929e3286a1c301e1669537757db1d358e559b9
taku0622 <hayatsutkm@gmail.com>
2022-09-24 23:56:49 +0900

    done

---
+++ src/silver/A0924.java
@@ -0,0 +1,7 @@
+package silver;
+
+public class A0924 {
+	public void test() {
+		System.out.println("A");
+	}
+}
---
+++ src/silver/C0924.java
@@ -0,0 +1,15 @@
+package silver;
+
+public class C0924 extends A0924{
+	public void test() {
+		System.out.println("C");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		A0924 a1 = new A0924();
+		A0924 a2 = new C0924();
+		a1 = (A0924)a2;
+		a1.test();
+	}
+
+}
