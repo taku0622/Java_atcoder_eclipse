e4c31768d9382ee015704526f80f4d2843a1c59c
taku0622 <hayatsutkm@gmail.com>
2023-04-03 02:24:08 +0900

    done

---
+++ src/jp/atcoder/ABC296_A.java
@@ -0,0 +1,42 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC296_A {
+
+	public static void main(String[] args) {
+		String str1;
+		String str2;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+			str2 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		String[] sa2 = str2.split("");
+		ArrayList<String> sl = new ArrayList<String>();
+		for(String s:sa2) {
+			sl.add(s);
+		}
+		aAlternately(il, sl);
+	}
+	private static void aAlternately(ArrayList<Integer> il,ArrayList<String> sl) {
+		for(int i=0;i<il.get(0)-1;i++) {
+//			System.out.println(i);
+			if(sl.get(i).equals(sl.get(i+1))) {
+				System.out.println("No");
+				return;
+			}
+		}
+		System.out.println("Yes");
+	}
+}
