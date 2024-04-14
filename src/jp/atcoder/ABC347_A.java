4585696a80b4ccaf54ed541568924f76ee2e41db
taku0622 <hayatsutkm@gmail.com>
2024-04-14 12:27:28 +0900

    done

---
+++ src/jp/atcoder/ABC347_A.java
@@ -0,0 +1,42 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC347_A {
+
+	public static void main(String[] args) {
+		String str1;
+		String str2;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+			str2 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il1 = new ArrayList<Integer>();
+		for(String s:sa) {
+			il1.add(Integer.parseInt(s));
+		}
+		String[] sa2 = str2.split(" ");
+		ArrayList<Integer> il2 = new ArrayList<Integer>();
+		for(String s:sa2) {
+			il2.add(Integer.parseInt(s));
+		}
+		aDivisible(il1,il2);
+	}
+	private static void aDivisible(ArrayList<Integer> il1, ArrayList<Integer> il2) {
+		String ans = "";
+		int numToDivide = il1.get(1);
+		for(int i=0;i<il2.size();i++) {
+			if(il2.get(i) % numToDivide == 0) {
+				ans += il2.get(i) / numToDivide + " ";
+			}
+		}
+		System.out.println(ans);
+	}
+}
