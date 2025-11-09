24da7edf66db7d5dc68fe36e685bf410e45b0d80
taku0622 <hayatsutkm@gmail.com>
2025-11-09 10:46:01 +0900

    done

---
+++ src/jp/atcoder/ABC429_A.java
@@ -0,0 +1,36 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC429_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		aGrandmaFootsteps(il);
+	}
+	private static void aGrandmaFootsteps(ArrayList<Integer> il) {
+		int n = il.get(0);
+		int m = il.get(1);
+		for(int i=0;i<n;i++) {
+			if(i < m) {
+				System.out.println("OK");
+			} else {
+				System.out.println("Too Many Requests");
+			}
+		}
+	}
+}
