66f939fe2be862f5e45848b041da28205982980b
taku0622 <hayatsutkm@gmail.com>
2025-12-29 14:02:02 +0900

    done

---
+++ src/jp/atcoder/ABC438_A.java
@@ -0,0 +1,32 @@
+package jp.atcoder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ABC438_A {
+
+	public static void main(String[] args) {
+		String str1;
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			str1 = br.readLine();
+		}catch (Exception e) {
+			System.out.println("Input Error!!");
+			return;
+		}
+		String[] sa = str1.split(" ");
+		ArrayList<Integer> il = new ArrayList<Integer>();
+		for(String s:sa) {
+			il.add(Integer.parseInt(s));
+		}
+		aFirstContestoftheYear(il);
+	}
+	private static void aFirstContestoftheYear(ArrayList<Integer> il) {
+		int d = il.get(0);
+		int f = il.get(1);
+		int sub = d - f;
+		int ans = 7 - (sub - (sub / 7) * 7);
+		System.out.println(ans);
+	}
+}
